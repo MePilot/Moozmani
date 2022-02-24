9ed7bf3528ae76ae52b158d825a0e9c99f72b9f3
BEST-KOMP\Admin <nebra2012@gmail.com>
2022-02-25 00:15:03 +0200

    latest

--- src/screens/welcome/WelcomeScreen.jsx
+++ src/screens/welcome/WelcomeScreen.jsx
@@ -1,5 +1,5 @@
 
-import './WelcomeScreen.css'
+import style from './WelcomeScreen.module.css'
 import backgroundImage from './background.png'
 import brandImage from './brand_icon.png'
 import arrowSVG from './arrow_left.svg'
@@ -9,24 +9,24 @@ function WelcomeScreen() {
   
     return (
 
-    <div className='container'>
-      <img className='brand_pic' src={brandImage} alt='Brand'/>
-      <img className='background_pic' src={backgroundImage} alt='Background'/>
-      <div className='entry'>
+    <div className={style.container}>
+      <img className={style.brand_pic} src={brandImage} alt='Brand'/>
+      <img className={style.background_pic} src={backgroundImage} alt='Background'/>
+      <div className={style.entry}>
         <p>כל מה שצריך <br/> לאירוע מושלם</p>
       
       </div>
-      <div className='entry_shadow'>
+      <div className={style.entry_shadow}>
         
       </div>
       <Link to="/phone">
-        <button className='btn_enter'>
-          <img className='arrow_icon1' src={arrowSVG} alt='arrow'/>
+        <button className={style.btn_enter}>
+          <img className={style.arrow_icon1} src={arrowSVG} alt='arrow'/>
             <span>מתכילים מכאן</span>
         </button>
       </Link>
      
-      <div className='btn_enter_shadow'>
+      <div className={style.btn_enter_shadow}>
        
       </div>
     </div>
