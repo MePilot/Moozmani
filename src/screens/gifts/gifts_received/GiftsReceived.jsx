b917a4046117ed3e5ac4a359cc81040bb5492c5a
BEST-KOMP\Admin <nebra2012@gmail.com>
2022-02-28 15:33:42 +0200

    latest

--- src/screens/gifts/gifts_received/GiftsReceived.jsx
+++ src/screens/gifts/gifts_received/GiftsReceived.jsx
@@ -1,35 +1,35 @@
 import style from './GiftsReceived.module.css'
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Container1 from '../../../containers/container_1/Container1';
 import searchSVG from './search.svg'
+import triangle from './triangle.svg'
 
 const contactList = [
-  {name:'אריאל', tel:'054-7568568', sum: '₪500'}, 
-  {name:'מיכאל', tel:'054-7536568',sum: '₪500'},
-  {name:'סלבה', tel:'053-7568568',sum: '₪500'}, 
-  {name:'ירון', tel:'058-7536568', sum: '₪500'},
-  {name:'אריאל', tel:'054-7568568', sum: '₪500'}, 
-  {name:'מיכאל', tel:'054-7536568', sum: '₪500'},
-  {name:'סלבה', tel:'053-7568568', sum: '₪500'}, 
-  {name:'ירון', tel:'058-7536568', sum: '₪500'},
-  {name:'אריאל', tel:'054-7568568', sum: '₪500'}, 
-  {name:'מיכאל', tel:'054-7536568', sum: '₪500'},
-  {name:'סלבה', tel:'053-7568568', sum: '₪500'}, 
-  {name:'ירון', tel:'058-7536568', sum: '₪500'}
+  {id:0, name:'אריאל', tel:'054-7568568', sum: '₪500'}, 
+  {id:1, name:'מיכאל', tel:'054-7536568',sum: '₪500'},
+  {id:2, name:'סלבה', tel:'053-7568568',sum: '₪500'}, 
+  {id:3,name:'ירון', tel:'058-7536568', sum: '₪500'},
+  {id:4,name:'אריאל', tel:'054-7568568', sum: '₪500'}, 
+  {id:5,name:'מיכאל', tel:'054-7536568', sum: '₪500'},
+  {id:6,name:'סלבה', tel:'053-7568568', sum: '₪500'}, 
+  {id:7,name:'ירון', tel:'058-7536568', sum: '₪500'},
+  {id:8,name:'אריאל', tel:'054-7568568', sum: '₪500'}, 
+  {id:9,name:'מיכאל', tel:'054-7536568', sum: '₪500'},
+  {id:10,name:'סלבה', tel:'053-7568568', sum: '₪500'}, 
+  {id:11,name:'ירון', tel:'058-7536568', sum: '₪500'}
 ]
 
 function GiftsReceived() {
-  const [state, setState] = useState('auto')
-  const [data, setData] = useState({})
-  const location = useLocation()
+  const [toggle, setToggle] = useState('')
+ console.log(toggle)
   const [search, setSearch] = useState('')
   const [contacts, setContacts] = useState(contactList)
   const navigate = useNavigate()
 
-  const handleData= (event)=> {
-    setData((prev)=>({...prev,[event.target.name]:event.target.value}))
-  }
+  useEffect(() => {
+    setToggle(contacts.reduce((acc, cur) => ({ ...acc, [cur.id]: false }), {}))
+  }, [])
 
   return (
     
@@ -42,7 +42,7 @@ bodyContent={
   <>
     
 <div className={style.sum_container}>
-  <p className={style.sum_label}>התקבל באשראי<br/><span className={style.sum}>₪24,000</span> </p>
+  <p className={style.sum_label}>:התקבל באשראי<br/><span className={style.sum}>₪24,000</span> </p>
 </div>
 
 <div className={style.reg_input_container}>
@@ -63,20 +63,39 @@ bodyContent={
   <ul className={style.payment_list}>
         {
           contacts.map((contact)=> {
+            
             return (
-              <li className={style.payment_list_item}>
-               
-                <label className={style.form_control}>
-                  <input type="checkbox" name="guestList" className={style.input_my} value={state.guestList}
-                    // onChange={
-                    //   event => handleData(event)
-                    // }
-                   />
-                 <span>{contact.name}</span>
-                </label>
+              <li className={style.payment_list_item} key={contact.id}
+             
+              onClick={()=>setToggle((toggle)=> ({...toggle, [contact.id]: !toggle[contact.id] }))}
+              >
 
-               {contact.tel}
-                 
+                <div  className={style.payment_list_item_container}>
+                <span>{contact.name}</span>
+               
+               <div className={style.payment_list_item_price}>
+               <span className={style.payment_list_item_sum}>{contact.sum}</span>
+               
+               <img 
+               className={toggle[contact.id] ? style.triangle_icon_down : style.triangle_icon_up}
+               src={triangle}
+               ></img>
+               
+               
+               </div>
+                  
+                </div>
+               
+                <div className={toggle[contact.id] ? style.payment_list_item_desc_down : style.payment_list_item_desc_up}>
+                     
+                     <p style={{margin:0, fontSize:'16px', fontWeight:'bold'}}>מזל טוב אח יקר!</p>
+                    <span>שיהיה אחלה זוגיות עם דניאל המהממת ושיהיה לכם רק טוב ובהצלחה אוהב, דקל וקנין</span>
+               
+                    <div className={style.payment_list_item_time}>
+                     01/02/22, 20:30
+                   </div>
+                </div>
+                  
                 </li>
             )
           })
