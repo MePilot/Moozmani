a9a8341a88e39e2c0900b7c19c4faca08dc50613
BEST-KOMP\Admin <nebra2012@gmail.com>
2022-03-17 17:36:31 +0200

    latest

--- src/screens/guests/guest_invitation_letter_preview/GuestInvitationPreview.jsx
+++ src/screens/guests/guest_invitation_letter_preview/GuestInvitationPreview.jsx
@@ -76,26 +76,83 @@ bodyContent={
     <p style={{ margin:0}}>02/02/20 ,יום רביעי בשעה 20:00</p>
     <p style={{margin:0, fontSize:'1rem'}}>  <img className={style.waze_icon}  src={wazeSVG}></img> מקום אירוע: אולמי אגדתא </p>
    
+    <div className={style.btn_container}>
+      <button className={arrive==='yes' ? style.btn_leftP : style.btn_left}
+       onClick={() => setArrive('yes')}
+      >
+        אגיע
+      </button>
+      <button className={arrive==='no' ? style.btn_middleP : style.btn_middle}
+       onClick={() => setArrive('no')}
+      >
+        לא אגיע
+      </button>
+      <button className={arrive==='maybe' ? style.btn_rightP : style.btn_right}
+       onClick={() => setArrive('maybe')}
+      >
+        מתלבט
+      </button>
     
+    </div>
+    <p style={{marginBottom:0, marginTop:'3%', fontSize:'1rem'}}>?כמה תגיעו</p>
+   
+    <div className={style.btn_container_2}>
+      <button className={style.btn_leftamount}
+      onClick={() => setGuestsAmount((prev)=>prev+1)}
+      >
+        +
+      </button>
+      <input className={style.telnumber}
+    
+      value={guestsAmount}
+      onChange={event => setGuestsAmount(event.target.value)}
+     onKeyPress={(event) => {
+      if (!/[0-9]/.test(event.key)) {
+        event.preventDefault();
+      }
+    }}
+   
+     type='tel'
+     inputMode='decimal'
+     autoComplete='tel'
+    
+     ></input>
+      <button className={style.btn_rightamount}
+       onClick={() => setGuestsAmount((prev)=>prev>0 ? prev-1 : prev)}
+      >
+       -
+      </button>
+    
+    </div>
    
     
 
     <div className='btn_group'>
-    
+    <button className={style.btn_pay}
+    onClick={()=>navigate('/giftsum')}
+      >
+       הענקת מתנה באשראי
+      </button>
+      <button
+     className={style.btn_finish}
+     onClick={()=>navigate('/guestresponcereceived')}
+     >
+       סיום
+      </button>
       <div className={style.share_container}>
-      <EmailShareButton url={window.location.host+'/guestinvitationmessage'}>
+      <EmailShareButton url={window.location.href}>
      <EmailIcon size={32} />
       </EmailShareButton>
-      <FacebookShareButton url={window.location.host+'/guestinvitationmessage'}>
+      <FacebookShareButton url={window.location.href}>
      <FacebookIcon size={32} />
       </FacebookShareButton>
-      <TwitterShareButton url={window.location.host+'/guestinvitationmessage'}>
+      <TwitterShareButton url={window.location.href}>
      <TwitterIcon size={32} />
       </TwitterShareButton>
-      <WhatsappShareButton url={window.location.host+'/guestinvitationmessage'}>
+      <WhatsappShareButton url={window.location.href}>
      <WhatsappIcon size={32} />
       </WhatsappShareButton>
-      <TelegramShareButton url={window.location.host+'/guestinvitationmessage'}>
+      <TelegramShareButton url={window.location.href}>
      <TelegramIcon size={32} />
       </TelegramShareButton>
       </div>
